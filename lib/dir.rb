1488f8da9999e43affec456be5f1ae93d41c347c
Jordan MacDonald <jordan@wastedintelligence.com>
2013-03-28 16:10:58 -0400

    Updated to use real_entries method.

--- lib/dir.rb
+++ lib/dir.rb
@@ -8,10 +8,7 @@ class Dir
   def self.size(path)
     size = 0
     full_path = File.expand_path(path) + '/'
-    Dir.entries(full_path).each do |entry|
-      # Skip meta-directories
-      next if entry == '.' || entry == '..'
-
+    Dir.real_entries(full_path).each do |entry|
       if File.file? full_path + entry
         size += File.size full_path + entry
       elsif File.directory? full_path + entry
